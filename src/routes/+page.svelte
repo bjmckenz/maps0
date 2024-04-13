31714e8154985f6696aca66126a118b9c316268d
bruce mckenzie <bjmckenz@gmail.com>
2024-04-12 19:12:36 -0700

    Added Marker

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,10 +1,12 @@
 <script>
 	import { browser } from '$app/environment';
-	//	import { Loader } from '@googlemaps/js-api-loader';
-
 	// this is not really public. but since it must be used on the client,
 	// it needs to be restricted via the Cloud Console (probably by domain)
 	import { PUBLIC_GOOGLE_MAPS_API_KEY } from '$env/static/public';
+
+	// Dynamically loaded constructors from Google Maps API
+	let Map;
+	let Marker;
 
 	let map_object; // the Google Maps object
 	let map_element; // the DOM element
@@ -15,32 +17,62 @@
 		mapId: 'SAMPLE_MAP_ID'
 	};
 
-    // "document" only exists in the browser; maps are client-side only
-    if (browser) {
-		((g) => { var h, a, k, p = 'The Google Maps JavaScript API', c = 'google', l = 'importLibrary', q = '__ib__', m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set(), e = new URLSearchParams(), u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement('script')); e.set('libraries', [...r] + ''); for (k in g) e.set( k.replace(/[A-Z]/g, (t) => '_' + t[0].toLowerCase()), g[k] ); e.set('callback', c + '.maps.' + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => (h = n(Error(p + ' could not load.'))); a.nonce = m.querySelector('script[nonce]')?.nonce || ''; m.head.append(a); })); d[l] ? console.warn(p + ' only loads once. Ignoring:', g) : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))); })
-        ({
+	// "document" only exists in the browser; maps are client-side only
+	if (browser) {
+		((g) => { var h, a, k, p = 'The Google Maps JavaScript API', c = 'google', l = 'importLibrary', q = '__ib__', m = document, b = window; b = b[c] || (b[c] = {}); var d = b.maps || (b.maps = {}), r = new Set(), e = new URLSearchParams(), u = () => h || (h = new Promise(async (f, n) => { await (a = m.createElement('script')); e.set('libraries', [...r] + ''); for (k in g) e.set( k.replace(/[A-Z]/g, (t) => '_' + t[0].toLowerCase()), g[k] ); e.set('callback', c + '.maps.' + q); a.src = `https://maps.${c}apis.com/maps/api/js?` + e; d[q] = f; a.onerror = () => (h = n(Error(p + ' could not load.'))); a.nonce = m.querySelector('script[nonce]')?.nonce || ''; m.head.append(a); })); d[l] ? console.warn(p + ' only loads once. Ignoring:', g) : (d[l] = (f, ...n) => r.add(f) && u().then(() => d[l](f, ...n))); })({
 			key: PUBLIC_GOOGLE_MAPS_API_KEY,
 			v: 'weekly'
 		});
 		map_element = document.getElementById('map');
 
 		(async () => {
-			const { Map } = await google.maps.importLibrary('maps');
-			const { AdvancedMarkerElement } = await google.maps.importLibrary('marker');
+			({ Map } = await google.maps.importLibrary('maps'));
+			({ Marker } = await google.maps.importLibrary('marker'));
 
 			map_object = new Map(map_element, initialMapDisplayOptions);
-
-			//const { AdvancedMarkerElement } = await loader.importLibrary('marker');
-			const marker = new AdvancedMarkerElement({
-				map: map_object,
-				position: { lat: 0, lng: 0 },
-				title: 'Greenwich'
-			});
 		})();
+	}
+
+	let lat;
+	let lng;
+	function get_current_position() {
+		navigator.geolocation.getCurrentPosition((position) => {
+			lat = position.coords.latitude;
+			lng = position.coords.longitude;
+		});
+	}
+	function get_center() {
+		const lat_lng_obj = map_object.getCenter();
+		({ lat, lng } = lat_lng_obj.toJSON());
+	}
+	function set_center() {
+		console.log('Setting center to', lat, lng);
+		map_object.setCenter({ lat: parseFloat(lat), lng: parseFloat(lng) });
+	}
+
+	let markerText = 'Hello, World!';
+	function create_marker() {
+		const marker = new Marker({
+			map: map_object,
+			position: { lat: parseFloat(lat), lng: parseFloat(lng) },
+			title: markerText
+		});
 	}
 </script>
 
 <div id="map"></div>
+<br />
+<label for="lat">Latitude:<input name="lat" type="text" bind:value={lat} /></label>
+<label for="lng">Longitude:<input name="lng" type="text" bind:value={lng} /></label>
+<br />
+<button on:click={() => get_current_position()}>Get Current Position</button>
+<button on:click={() => get_center()}>Get Map Center</button>
+<button on:click={() => set_center()}>Set Map Center</button>
+<br />
+<button on:click={() => create_marker()}>Create Marker</button>
+<label for="markerText"
+	>Marker text:<input name="markerText" type="text" bind:value={markerText} /></label
+>
 
 <style>
 	#map {
